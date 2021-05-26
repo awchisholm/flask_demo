ae161f2b721cd0d8d9b7cab29a46c60efd93007d
Andrew Chisholm <awchisholm@gmail.com>
2021-05-26 12:38:15 +0100

    typo

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'This is an example Flask application serving static content.'
+    return 'This is an example Flask application serving static content'
