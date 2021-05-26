5223aadba637b49ac5983d95758791b3489dfe45
Andrew Chisholm <awchisholm@gmail.com>
2021-05-26 13:30:03 +0100

    save

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return 'This is an example Flask application serving static content by Andrew'
 
 if __name__ == '__main__':
